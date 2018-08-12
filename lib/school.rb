53fdbdf526f80c49beff48872901878685842ce8
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-12 21:55:36 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :school, :add_student
+
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.sort_by {|grade, student| [student, grade]}
+  end
+
+end
